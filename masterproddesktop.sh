2a6e7bc52466d04d80ce8e4845086d00d29e69be
nafizzz <nhremon8181@gmail.com>
2021-07-18 15:37:57 +0600

    master in prod config bash

---
+++ masterproddesktop.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+	#saved the file at root, changed permission (chmod +x file.sh)
+	cd TigerIT/projects/communicator-desktop-pwa/ #directory
+
+	clear
+	branch="master"
+	environment="prod"
+	version="1.1.73"
+	#read -p "branch name : " branch
+	echo "branch name : $branch"
+	#read -p "environment  : loc/prod? " environment
+	echo "environment  : $environment"
+	#read -p "version  : " version
+	echo "version  : $version" 
+	#read -p "Do you want to install npm  : y/n " isnpm
+	#echo ""
+	#if [ $isnpm = y ]
+	#	then npm i
+	#echo "npm i"
+	#echo "<<<------------------------ npm updated -------------------------->>>"
+	#echo ""
+	#fi
+	d=$(date "+%d%b%H%M")
+	Appversion="$version-$branch$environment$d"
+	#Appversion="$version-$performanceJuly12$environment$d"  #for branch contains /
+	#d=$(date +%Y-%m-%d)
+        #echo " current date is $d"
+
+	echo "setting up build for $branch branch, $Appversion version on $environment environment"
+	echo ""
+
+	#git
+	echo "<<<------------------------ -----git------ -------------------------->>>"
+	git fetch
+	git reset --hard
+	git checkout $branch
+	git pull
+	echo ""
+	echo "<<<------------------------ environment setup completed -------------------------->>>"
+	#setting up environment
+	#first you need to have to create two environment files named lenvironment.ts (local config) and penvironment.ts(prod config) and save at src/environments
+	cd src/environments
+	rm environment.ts
+	if [ $environment = loc ]
+		then cp lenvironment.ts environment.ts
+	elif [ $environment = prod ]
+		then cp penvironment.ts environment.ts
+	fi
+	cd ../../electron/
+	#edit package.json version with the input
+	sed -i '3d' package.json
+	sed -i '3i   "version": "'"$Appversion"'",' package.json
+	echo ""
+	echo "<<<------------------------ -----building the app------ -------------------------->>>"	
+	#build command for specific OS, here is for linux
+	ionic cap sync electron
+	npm run electron:build-linux
+	cd dist/
+	sudo snap remove commchatbeta
+	sudo snap install --dangerous commchatbeta_${Appversion}_amd64.snap
+	commchatbeta 
