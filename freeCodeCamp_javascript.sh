ac8ffb150e06bf954db41471ab69af737a9b5932
nafizzzzzzzzzz <33452602+nafizzzzzzzzzz@users.noreply.github.com>
2022-11-15 10:58:36 +0600

    Update freeCodeCamp_javascript.sh
    More efficient, Just need to select problem title, will automatically take input from text editor field next to the problem and will make a file according the problem title, will save into local directory according the title and will push into git automatically into it's remote repo.

--- freeCodeCamp_javascript.sh
+++ freeCodeCamp_javascript.sh
@@ -1,12 +1,69 @@
+delay=0.1
+delay2=0.2
+sleep 4
+#sleep 2
+sleep $delay
+xdotool key ctrl+c
+sleep $delay
+clipboard_content=`xclip -o -selection clipboard`
+echo "$clipboard_content"
+
+sleep $delay
+eval $(xdotool getmouselocation --shell)
+echo $X $Y
+X=$((X+600))
+echo $X $Y
+sleep $delay
+xdotool mousemove $X $Y click 1 
+sleep $delay
+xdotool key ctrl+a
+sleep $delay
+xdotool key ctrl+c
+sleep $delay
+solution=`xclip -o -selection clipboard`
+
 cd /home/tigerit/TigerIT/projects/Coding/freeCodeCamp/JavaScript\ Algorithms\ and\ Data\ Structures/
-param="'$*'"
-echo $param
+#param="'$*'"
+#echo $param
 oldstr=" "
 newstr="_"
-result=$(echo $param | sed "s/$oldstr/$newstr/g")
+result=$(echo $clipboard_content | sed "s/$oldstr/$newstr/g")
 result=$(echo $result | sed "s/'//g")
 echo $result
-nano $result
+echo $solution
+
+
+
+
+
+
+
+
+
+
+sleep $delay
+xdotool key Alt+Tab
+sleep $delay
+xdotool key ctrl+shift+t
+sleep $delay2
+xdotool type "cd /home/tigerit/TigerIT/projects/Coding/freeCodeCamp/JavaScript\ Algorithms\ and\ Data\ Structures/"
+xdotool key Return
+
+
+
+sleep $delay
+xdotool type "nano $result"
+sleep $delay
+xdotool key Return
+
+sleep $delay
+xdotool key ctrl+shift+v
+sleep $delay
+xdotool key ctrl+s
+sleep $delay
+xdotool key ctrl+x
+sleep $delay
+xdotool key ctrl+shift+W
 
 git add $result
 git commit -m "$result solved"
